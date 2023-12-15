29e7799bf1a8a099be7cb0a0de2922b0dc7f4dc8
kaleck hamm <kaleckh@gmail.com>
2023-12-15 13:08:58 -0700

    debugging need to commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
-import { TonerProvider } from "../app/toner/index";
+import { TonerProvider } from "../app/providers/toner/index";
 
 const inter = Inter({ subsets: ['latin'] })
 
---
+++ src/app/providers/cart/index.js
@@ -0,0 +1,54 @@
+"use client";
+
+import { createContext, useEffect, useState } from "react";
+
+export const CartContext = createContext(undefined);
+
+export const CartProvider = ({ children }) => {
+  let [realPrice, setRealPrice] = useState();
+  let [cart, setCart] = useState([]);
+  let [tonerOem, setTonerOem] = useState();
+  let [cartLook, setCartLook] = useState([]);
+  let [cardInfo, setCardInfo] = useState({});
+  let [billingInfo, setBillingInfo] = useState({});
+  let [personInfo, setPersonInfo] = useState({});
+  let [totalAmount, setTotalAmount] = useState();
+
+
+
+  useEffect(() => {
+    if (cart.length > 0) {
+      localStorage.setItem("cart", JSON.stringify(cart))
+    }
+
+  }, [cart]);
+
+  useEffect(() => {
+    var price = 0
+    cart.map((item) => {
+      price = price + item.price * item.quantity
+    })
+    price = price + 2.99
+    setTotalAmount(price.toFixed(2))
+  }, [cart])
+
+  useEffect(() => {
+    if (realPrice !== undefined) {
+      localStorage.setItem("realPrice", JSON.stringify(realPrice))
+    }
+  }, [realPrice])
+
+  useEffect(() => {
+    setCart(JSON.parse(localStorage.getItem("cart")) ?? [])
+    setTonerOem((localStorage.getItem("tonerOem")))
+
+  }, [])
+
+
+
+  return (
+    <CartContext.Provider value={{ cart, setCart, setRealPrice, cardInfo, setCardInfo, personInfo, setPersonInfo, totalAmount, billingInfo, setBillingInfo, tonerOem, setTonerOem }}>
+      {children}
+    </CartContext.Provider>
+  );
+};
