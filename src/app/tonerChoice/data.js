e999f9f926fd483e03c4f8886a46024f05061113
kaleck hamm <kaleckh@gmail.com>
2024-01-29 16:49:14 -0700

    test

--- src/app/tonerChoice/data.js
+++ src/app/tonerChoice/data.js
@@ -7,6 +7,7 @@ import styles from "../styles/fileChoice.module.css";
 import Footer from "../components/Footer";
 import Link from "next/link";
 import { CartContext } from "../providers/cart";
+import { Suspense } from 'react'
 import { useSearchParams } from "next/navigation";
 // import BreadCrumbs from "../components/BreadCrumbs";
 
@@ -18,14 +19,14 @@ const TonerChoice = (props) => {
     const [aboveOne, setAboveOne] = useState(false);
     const [quantity, setQuantity] = useState(1);
     const [toner, setToner] = useState("");
-    const [name, setName] = useState("");    
+    const [name, setName] = useState("");
     const [cartAccess, setCartAccess] = useState(true);
     const [email, setEmail] = useState("");
     const [number, setNumber] = useState("");
     const [message, setMessage] = useState("");
     const [orderId, setOrderId] = useState("");
     const [newPrice, setNewPrice] = useState();
-    const[token, setToken] = useState()
+    const [token, setToken] = useState()
     const tawkMessengerRef = useRef();
 
     // useEffect(() => {
@@ -111,131 +112,133 @@ const TonerChoice = (props) => {
         getProducts()
     }, [])
 
-    
+
     useEffect(() => {
         if (localStorage) setToken(JSON.parse(localStorage.getItem("token")))
-      }, [])
+    }, [])
 
     return (
-        <div className={styles.main}>            
-            <Header />
-            <div className={styles.mainContent}>
-                <div className={styles.center}>
-                    <div className={styles.column}>
+        <div className={styles.main}>
+            <Suspense>
+                <Header />
+                <div className={styles.mainContent}>
+                    <div className={styles.center}>
+                        <div className={styles.column}>
 
-                        <div className={styles.titleSmall}> OEM #: {toner[0]?.oemNos[0].oemNo}</div>
-                        <div className={styles.imageContainer}>
-                            <Image src={toner[0]?.images[0]} width={300} height={250} />
+                            <div className={styles.titleSmall}> OEM #: {toner[0]?.oemNos[0].oemNo}</div>
+                            <div className={styles.imageContainer}>
+                                <Image src={toner[0]?.images[0]} width={300} height={250} />
+                            </div>
                         </div>
-                    </div>
-                    <div className={styles.centerFeature}>
-                        <div className={styles.aContainer}>
-                            <div className={styles.titleLarge}>{toner[0]?.title}</div>
-                            <div className={styles.something}>
-                                <div className={styles.titleSmall}>
-                                    <div className={styles.small}>
-                                        ${aboveOne ? newPrice : toner[0]?.serviceLevels[0].price}
+                        <div className={styles.centerFeature}>
+                            <div className={styles.aContainer}>
+                                <div className={styles.titleLarge}>{toner[0]?.title}</div>
+                                <div className={styles.something}>
+                                    <div className={styles.titleSmall}>
+                                        <div className={styles.small}>
+                                            ${aboveOne ? newPrice : toner[0]?.serviceLevels[0].price}
+                                        </div>
+                                    </div>
+                                    <div className={styles.line}></div>
+                                    <div style={{ color: "black" }}>Features:</div>
+                                    <div className={styles.flex}>
+                                        <div className={styles.detailColumn}>
+                                            <div className={styles.titleSmall}>
+                                                Model: <div className={styles.small}>{oem}</div>{" "}
+                                            </div>
+                                            <div className={styles.titleSmall}>
+                                                Yield: <div className={styles.small}>{toner[0]?.yield}</div>{" "}
+                                            </div>
+                                        </div>
+                                        <div className={styles.detailColumn}>
+                                            <div className={styles.titleSmall}>
+                                                Shipping Weight: <div className={styles.titleSmall}>{toner[0]?.productBoxDimensions.weight}</div>
+                                            </div>
+
+                                            <div className={styles.titleSmall}>
+                                                Condition: <div className={styles.small}>New</div>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
                                 <div className={styles.line}></div>
-                                <div style={{ color: "black" }}>Features:</div>
-                                <div className={styles.flex}>
-                                    <div className={styles.detailColumn}>
-                                        <div className={styles.titleSmall}>
-                                            Model: <div className={styles.small}>{oem}</div>{" "}
-                                        </div>
-                                        <div className={styles.titleSmall}>
-                                            Yield: <div className={styles.small}>{toner[0]?.yield}</div>{" "}
-                                        </div>
+                            </div>
+                            <div style={{ paddingTop: "10px", marginBottom: "10px", paddingBottom: "20px", display: "flex", width: "35%", paddingRight: "10px" }} className={styles.titleSmall}>
+                                Qty
+                                <div className={styles.centerBottom}>
+                                    <div className={styles.centerPlus}>
+                                        <div onClick={() => {
+                                            setQuantity(quantity + 1)
+                                        }} className={styles.plus}>+</div>
                                     </div>
-                                    <div className={styles.detailColumn}>
-                                        <div className={styles.titleSmall}>
-                                            Shipping Weight: <div className={styles.titleSmall}>{toner[0]?.productBoxDimensions.weight}</div> 
-                                        </div>
+                                    <input style={{ textAlign: "center" }}
+                                        onChange={(event) => {
+                                            setQuantity(event.target.value);
+                                        }}
+                                        className={styles.number}
+                                        placeholder={quantity}
+                                        type="number"
+                                    />
+                                    <div className={styles.centerPlus}>
+                                        <div onClick={() => {
+                                            if (quantity > 1) {
+                                                setQuantity(quantity - 1)
+                                            }
 
-                                        <div className={styles.titleSmall}>
-                                            Condition: <div className={styles.small}>New</div>
-                                        </div>
+                                        }} className={styles.minus}>-</div>
                                     </div>
                                 </div>
                             </div>
-                            <div className={styles.line}></div>
-                        </div>
-                        <div style={{ paddingTop: "10px", marginBottom: "10px", paddingBottom: "20px", display: "flex", width: "35%", paddingRight: "10px" }} className={styles.titleSmall}>
-                            Qty
-                            <div className={styles.centerBottom}>
-                                <div className={styles.centerPlus}>
-                                    <div onClick={() => {
-                                        setQuantity(quantity + 1)
-                                    }} className={styles.plus}>+</div>
-                                </div>
-                                <input style={{ textAlign: "center" }}
-                                    onChange={(event) => {
-                                        setQuantity(event.target.value);
-                                    }}
-                                    className={styles.number}
-                                    placeholder={quantity}
-                                    type="number"
-                                />
-                                <div className={styles.centerPlus}>
-                                    <div onClick={() => {
-                                        if (quantity > 1) {
-                                            setQuantity(quantity - 1)
-                                        }
+                            <div className={styles.buttonRow}>
+                                <button
+                                    className={styles.buttonBlue}
+                                    onClick={(e) => {
+                                        getOrderData().then(() => {
 
-                                    }} className={styles.minus}>-</div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className={styles.buttonRow}>
-                            <button
-                                className={styles.buttonBlue}
-                                onClick={(e) => {
-                                    getOrderData().then(() => {
-
-                                        setOrderData().then(() => {
-                                            sendSuccessEmail(e);
-                                            createDistribution();
+                                            setOrderData().then(() => {
+                                                sendSuccessEmail(e);
+                                                createDistribution();
+                                            });
                                         });
-                                    });
-                                    setOrderData();
-                                }}
-                            >
-                                Buy Now
-                            </button>
-                            <Link href={"/carts"}>
-                                <button className={styles.button3}>
-                                    {cartAccess ? (
-                                        <div
-                                            onClick={() => {
-                                                const updatedCart = [
-                                                    ...cart,
-                                                    {
-                                                        title: toner[0]?.title,
-                                                        oem: toner[0]?.oemNos[0].oemNo,
-                                                        price: toner[0]?.serviceLevels[0].price,
-                                                        quantity: quantity,
-                                                        image: toner[0]?.images[0],
-                                                    },
-                                                ];
-                                                setCart(updatedCart);
-                                                // JSON.stringify(localStorage.setItem("cart", updatedCart))
-
-                                            }}
-                                        >
-                                            Add To Cart
-                                        </div>
-                                    ) : (
-                                        <div>Already Added</div>
-                                    )}
+                                        setOrderData();
+                                    }}
+                                >
+                                    Buy Now
                                 </button>
-                            </Link>
+                                <Link href={"/carts"}>
+                                    <button className={styles.button3}>
+                                        {cartAccess ? (
+                                            <div
+                                                onClick={() => {
+                                                    const updatedCart = [
+                                                        ...cart,
+                                                        {
+                                                            title: toner[0]?.title,
+                                                            oem: toner[0]?.oemNos[0].oemNo,
+                                                            price: toner[0]?.serviceLevels[0].price,
+                                                            quantity: quantity,
+                                                            image: toner[0]?.images[0],
+                                                        },
+                                                    ];
+                                                    setCart(updatedCart);
+                                                    // JSON.stringify(localStorage.setItem("cart", updatedCart))
+
+                                                }}
+                                            >
+                                                Add To Cart
+                                            </div>
+                                        ) : (
+                                            <div>Already Added</div>
+                                        )}
+                                    </button>
+                                </Link>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
 
-            <Footer />
+                <Footer />
+            </Suspense>
         </div>
     );
 };
